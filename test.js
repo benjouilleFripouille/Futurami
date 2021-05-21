ea3ba35baa2de2218723c77d4ca60f683d72a1dc
Marina <velascomarina801@outlook.fr>
2021-05-21 15:32:51 +0200

    compteur de clic sur lien

--- test.js
+++ test.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function(){
 
-    
+    var clicCarte1 = 0;
+    var clicCarte2 = 0;
+
+    $('#carte1').on('click', function(){
+        clicCarte1++;
+        console.log(clicCarte1);
+        $('#1').text("Vous a cliqué " +clicCarte1+ " fois sur la 1ère carte !");
+    })
+
+    $('#carte2').on('click', function(){
+        clicCarte2++;
+        console.log(clicCarte2);
+        $('#2').text("Vous a cliqué " +clicCarte2+ " fois sur la 2ème carte !");
+    })
 
 })
