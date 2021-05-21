b3382e23abea27234009b00d4166bb49032da26f
Marina <velascomarina801@outlook.fr>
2021-05-21 14:56:22 +0200

    tests

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-var d1 = 0;
-var d2 = 0;
-var d3 = 0;
-var d4 = 0;
-var d5 = 0;
-var d6 = 0;
+"use strict"
 
+$(document).ready(function(){
+
+    var d1 = 0;
+    var d2 = 0;
+    var d3 = 0;
+    var d4 = 0;
+    var d5 = 0;
+    var d6 = 0;
+    
+
+})
