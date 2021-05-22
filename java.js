0b6a8463b85ef3b723e0618f7672c6ef780d9c13
Jules Mesnil <jules.mesnil@gmail.com>
2021-05-22 12:06:29 +0200

    burger

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", function(){
+    navbar.classList.toggle("change");
+});
+
 $(document).ready(function(){
 
     $(document).load()
